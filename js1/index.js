50efcb6779b13ea20411335c770d2c7f885749cf
Dumie1 <dumiedumara@gmail.com>
2018-09-09 13:51:52 +0200

    added solutions for freeCodeCamp

---
+++ js1/index.js
@@ -0,0 +1,31 @@
+/* ===> Use Multiple Conditional Ternary Operators
+
+function checkSign(num) {
+    return (num === 0) ?'zero': (num < 0) ?'negative': 'positive';
+}
+checkSign(10);*/
+
+// =================================================
+/* ===> Use the Conditional Ternary Operator
+
+function checkEqual(a, b) {
+    return a === b ? true : false;
+}
+checkEqual(1, 2);*/
+
+// =================================================
+/* ===> Manipulate Arrays With pop()
+var threeArr = [1, 4, 6];
+var oneDown = threeArr.pop();
+console.log(oneDown); // Returns 6
+console.log(threeArr); // Returns [1, 4]
+
+var myArray = [["John", 23], ["cat", 2]];
+var removedFromMyArray = myArray.pop();*/
+
+// ==================================================
+/* ===>Manipulate Arrays With shift()
+
+var myArray = [["John", 23], ["dog", 3]];
+var removedFromMyArray = myArray.shift();*/
+
