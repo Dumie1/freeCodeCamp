7eafc5c3c3060ddfed32de5b4342ac14387cf624
Dumie1 <dumiedumara@gmail.com>
2018-09-12 01:09:56 +0200

    some more code ...

--- js1/index.js
+++ js1/index.js
@@ -25,8 +25,69 @@ for (var i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }*/
 
+// ====================================================
+/* ===> Nesting For Loops
 
+function multiplyAll(arr) {
+var product = 1;
+// Only change code below this line
+for (var i=0; i<arr.length; i++){
+for(var j=0; j<arr[i].length; j++){
+product = product * arr[i][j];
+}
+}
+// Only change code above this line
+return product;
+}
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);*/
 
+// ==================================================
+/* ===> Iterate with JavaScript Do...While Loops
+
+var myArray = [];
+var i = 10;
+do {
+  myArray.push(i);
+  i++;
+} while (i < 5);*/
+
+// ==============================================
+/* ===> Profile Lookup
+
+for (var x = 0; x < contacts.length; x++){
+    if (contacts[x].firstName === name) {
+        if (contacts[x].hasOwnProperty(prop)) {
+            return contacts[x][prop];
+        } else {
+            return "No such property";
+        }
+    }
+}
+return "No such contact";*/
+
+// ================================================
+/* Generate Random Fractions with JavaScript
+
+function randomFraction() {
+  // Only change code below this line.
+  var result = 0;
+  // Math.random() can generate 0. We don't want to     return a 0,
+  // so keep generating random numbers until we get one     that isn't 0
+  while (result === 0) {
+    result = Math.random();
+  }
+  return result;
+  // Only change code above this line.
+}*/
+
+// ===============================================
+/* ===> Generate Random Whole Numbers with JavaScript
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);
+}*/
 
 
 
