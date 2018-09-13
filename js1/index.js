062e5fed1378e5adf739bd9a13308f2c3a68b6fe
Dumie1 <dumiedumara@gmail.com>
2018-09-13 20:21:41 +0200

    js more objects

--- js1/index.js
+++ js1/index.js
@@ -627,8 +627,26 @@ var myDog = {
 
 // ==================================================
 /* ===> Accessing Object Properties with Dot Notation
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+// Only change code below this line
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line*/
 
-
+// ==================================================
+/* ===> 
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+// Only change code below this line
+var entreeValue = testObj['an entree'];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
 */
 
 
