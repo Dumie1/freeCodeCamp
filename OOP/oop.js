4078ff8c666d1a2bc71383c62341ca53d3b71d27
Dumie1 <dumiedumara@gmail.com>
2018-09-27 21:42:46 +0200

    added new objects exersises

---
+++ OOP/oop.js
@@ -0,0 +1,40 @@
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+-----Create a Basic JavaScript Object------
+
+let dog = {
+  name : 'popy',
+  numLegs : 2
+};*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+-------- Use Dot Notation to Access the Properties of an Object-------
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+console.log(dog.name , dog.numLegs);*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+---------Create a Method on an Object------------
+let duck = {
+  name: "Aflac",
+  numLegs: 2,
+  sayName: function() {return "The name of this duck is " + duck.name + ".";}
+};
+duck.sayName();
+// Returns "The name of this duck is Aflac."
+
+------------------------------------------------
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function() {
+    return "This dog has " + dog.numLegs + " legs.";
+  }
+};
+dog.sayLegs();*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+*/
