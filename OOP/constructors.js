bcf6e90290845d8faa1589223739d2f190bfb0e8
Dumie1 <dumiedumara@gmail.com>
2018-09-28 15:33:16 +0200

    added new constructor function file

---
+++ OOP/constructors.js
@@ -0,0 +1,34 @@
+/*
+
+* Constructors are defined with a capitalized name to distinguish them from
+ other functions that are not constructors.
+* Constructors use the keyword this to set properties of the object they will 
+create.Inside the constructor, this refers to the new object it will create.
+* Constructors define properties and behaviors instead of returning a value as 
+other functions might.
+
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+---------Define a Constructor Function----------
+are functions that create new objects. They define properties and behaviors
+that will belong to the new object. an example below...
+
+function Bird() {
+  this.name = "Albert";
+  this.color = "blue";
+  this.numLegs = 2;
+}
+
+----------------------------------
+function Dog (){
+  this.name = 'Pol',
+  this.color = 'brown',
+  this.numLegs = 2
+}*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+-----------Use a Constructor to Create Objects------------
+
+
+*/
