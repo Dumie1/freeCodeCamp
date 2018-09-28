dbd613b28fc13a48a243dce8bd68401a7806d09c
Dumie1 <dumiedumara@gmail.com>
2018-09-28 17:35:06 +0200

    done

--- OOP/constructors.js
+++ OOP/constructors.js
@@ -235,6 +235,19 @@ for (let property in beagle) {
 }*/
 
 /* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
---------------------
+----------understanding constructor----------
+function Dog(name) {
+  this.name = name;
+}
 
-*/
+// Add your code below this line
+function joinDogFraternity(candidate) {
+  if(candidate.constructor === Dog) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}*/
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
