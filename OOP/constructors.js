4cb22d6143fc8a70ece0519f6e061049200555e7
Dumie1 <dumiedumara@gmail.com>
2018-09-28 16:57:50 +0200

    more code

--- OOP/constructors.js
+++ OOP/constructors.js
@@ -30,5 +30,117 @@ function Dog (){
 /* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 -----------Use a Constructor to Create Objects------------
 
+function Bird() {
+  this.name = "Albert";
+  this.color = "blue";
+  this.numLegs = 2;
+  // "this" inside the constructor always refers to the object being created
+}
+let blueBird = new Bird();
+
+-------------------------------------------
+
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Add your code below this line
+let hound = new Dog();*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+------------Extend Constructors to Receive Arguments-----------
+To more easily create different Bird objects, you can design your Bird 
+constructor to accept parameters:
+
+function Bird(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 2;
+}
+----------------------------------------------
+
+function Dog(name,color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 4
+}
+let terrier = new Dog('Bob', 'browm');*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+--------Verify an Object's Constructor with instanceof--------
+instanceof allows you to compare an object to a constructor, returning 
+true or false based on whether or not that object was created with the 
+constructor.
+
+let Bird = function(name, color) {
+  this.name = name;
+  this.color = color;
+  this.numLegs = 2;
+}
+let crow = new Bird("Alexis", "black");
+crow instanceof Bird; // => true
+
+-----------------------------------------------------------
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+let myHouse = new House(3);
+myHouse instanceof House;*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+-----Understand Own Properties-------
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+let duck = new Bird("Donald");
+let canary = new Bird("Tweety");
+*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+--------Understand Own Properties-----------
+
+name and numLegs are called own properties, because they are defined directly 
+on the instance object. That means that duck and canary each has its own separate 
+copy of these properties.
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+let duck = new Bird("Donald");
+let canary = new Bird("Tweety");*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+---------------------
+let ownProps = [];
+for (let property in duck) {
+  if(duck.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+console.log(ownProps); // prints [ "name", "numLegs" ]
+
+-------------------------------------
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+let canary = new Bird("Tweety");
+let ownProps = [];
+// Add your code below this line
+for(let property in canary) {
+  if(canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}*/
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----------Use Prototype Properties to Reduce Duplicate Code-------
+
+
 
 */
