31360c6ddee16980ba2934d3df85d9bafd9f4e5a
Dumie1 <dumiedumara@gmail.com>
2018-09-30 06:54:28 +0200

    done

--- OOP/json.js
+++ OOP/json.js
@@ -36,6 +36,14 @@ user = JSON.parse(user);
 alert( user.friends[1] ); // 1
 */
 
-/*
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+------Turn the object into JSON and back---------
+Turn the user into JSON and then read it back into another variable.
 
+let user = {
+  name: "John Smith",
+  age: 35
+};
+let user2 = JSON.parse(JSON.stringify(user));
+console.log(user2);
 */
