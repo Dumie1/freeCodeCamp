5a349c9f337b9aa7379789e1e28452b204e7857b
Dumie1 <dumiedumara@gmail.com>
2018-10-02 17:09:17 +0200

    added new promise.js file

--- OOP/strings.js
+++ OOP/strings.js
@@ -11,10 +11,44 @@ Output - string
 
 typeof stringObject;
 Output - object
+
+Using square bracket notation, we can access any character in the string.
+"How are you?"[5]; output - r
+
+use the charAt() method to return the character using the index number as a parameter.
+"How are you?".charAt(5); output - r
+
+slice()
+The first parameter will be the starting index number, and the second parameter 
+will be the index number where it should end.
+"How are you?".slice(8, 11); output - you
+
+If a second parameter is not included, slice() will return everything from the
+parameter to the end of the string.
+ "How are you?".slice(8); output - you?
+
+"How are you?".toUpperCase(); output - HOW ARE YOU?
+
+toLowerCase() will convert all characters to lowercase characters. 
+"How are you?".toLowerCase();  output - how are you?
+
+const originalString = "How are you?";
+// Split string by whitespace character
+const splitString = originalString.split(" ");
+console.log(splitString);
+
+
+*/ 
+
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+--------------
+
 */
 
 /* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+----useful links below --------
 
+https://www.digitalocean.com/community/tutorials/how-to-index-split-and-manipulate-strings-in-javascript
 
 
 */
