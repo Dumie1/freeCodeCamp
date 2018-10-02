8a55aa84b8f765aeda24e2656b65864e0a7a990e
Dumie1 <dumiedumara@gmail.com>
2018-10-02 10:59:05 +0200

    new ES6 file added

---
+++ OOP/ES6.js
@@ -0,0 +1,15 @@
+/* &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+-----Use Arrow Functions to Write Concise Anonymous Functions----
+
+const magic = () => {
+  "use strict";
+  return new Date();
+};
+-----------------------------------------------
+const myFunc = function() {
+  const myVar = "value";
+  return myVar;
+}
+----------------------------------------------
+
+*/
